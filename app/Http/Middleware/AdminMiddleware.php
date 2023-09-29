11256fc9869dabf62069d6d7bbbe36fdaa87e4b5
Manuel Alzate <maalzates@gmail.com>
2023-09-29 14:17:39 -0500

    Created middlewares for protecting admin routes, and authenticated user routes

---
+++ app/Http/Middleware/AdminMiddleware.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AdminMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+                // Check if the user is authenticated and has the admin role
+        if (Auth::check() && Auth::user()->hasRole('Admin')) {
+            return $next($request);
+        }
+
+        // If not, redirect to the campaigns route
+        return redirect('/campaigns');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -4,11 +4,10 @@ use App\Http\Controllers\CampaignController;
 use App\Http\Controllers\CommentController;
 use App\Http\Controllers\ItemController;
 use App\Http\Controllers\UserController;
+use App\Http\Middleware\AdminMiddleware;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
-
-
 
 /*
 |--------------------------------------------------------------------------
@@ -42,6 +41,85 @@ use Inertia\Inertia;
 
 Route::redirect('/', 'login');
 
+// // ITEMS
+// // List show
+// Route::get('/items', [ItemController::class, 'index'])->name('items.index');
+// // Create record route
+// Route::get('/items/create', [ItemController::class, 'create'])->name('items.create');
+// // Store the created record route
+// Route::post('items', [ItemController::class, 'store'])->name('items.store');
+// // Edit view route
+// Route::get('items/{item}/edit', [ItemController::class, 'edit'])->name('items.edit');
+// // Update edited information route
+// Route::put('items/{item}', [ItemController::class, 'update'])->name('items.update');
+// // Delete record route
+// Route::delete('items/{item}', [ItemController::class, 'destroy'])->name('items.destroy');
+
+
+// // CAMPAIGNS
+// // List show
+// Route::get('/campaigns', [CampaignController::class, 'index'])->name('campaigns.index');
+// // Create record route
+// Route::get('/campaigns/create', [CampaignController::class, 'create'])->name('campaigns.create');
+// // Store the created record route
+// Route::post('campaigns', [CampaignController::class, 'store'])->name('campaigns.store');
+// // Show view route
+// Route::get('campaigns/{campaign}/show', [CampaignController::class, 'show'])->name('campaigns.show');
+// // Edit view route
+// Route::get('campaigns/{campaign}/edit', [CampaignController::class, 'edit'])->name('campaigns.edit');
+// // Update edited information route
+// Route::put('campaigns/{campaign}', [CampaignController::class, 'update'])->name('campaigns.update');
+// // Delete record route
+// Route::delete('campaigns/{campaign}', [CampaignController::class, 'destroy'])->name('campaigns.destroy');
+
+// // COMMENTS 
+// Route::get('/comments/create', [CommentController::class, 'create'])->name('comments.create');
+// //Store
+// Route::post('comments', [CommentController::class, 'store'])->name('comments.store');
+// // Delete
+// Route::delete('comments', [CampaignController::class, 'destroy'])->name('comments.destroy')->middleware('web');;
+// // USERS
+// Route::resource('users', UserController::class)->names('users');
+
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return Inertia::render('Dashboard');
+    })->name('dashboard');
+
+// CAMPAIGNS
+// List show
+Route::get('/campaigns', [CampaignController::class, 'index'])->name('campaigns.index');
+
+});
+
+
+// ONLY AUTHENTICATED USERS WITH ADMIN ROLE 
+Route::middleware([
+    'auth:sanctum', 
+    config('jetstream.auth_session'),
+    'verified',
+    AdminMiddleware::class
+])->group(function () {
+    
+    // CAMPAIGNS 
+// Create record route
+Route::get('/campaigns/create', [CampaignController::class, 'create'])->name('campaigns.create');
+// Store the created record route
+Route::post('campaigns', [CampaignController::class, 'store'])->name('campaigns.store');
+// Show view route
+Route::get('campaigns/{campaign}/show', [CampaignController::class, 'show'])->name('campaigns.show');
+// Edit view route
+Route::get('campaigns/{campaign}/edit', [CampaignController::class, 'edit'])->name('campaigns.edit');
+// Update edited information route
+Route::put('campaigns/{campaign}', [CampaignController::class, 'update'])->name('campaigns.update');
+// Delete record route
+Route::delete('campaigns/{campaign}', [CampaignController::class, 'destroy'])->name('campaigns.destroy');
+
 // ITEMS
 // List show
 Route::get('/items', [ItemController::class, 'index'])->name('items.index');
@@ -56,23 +134,6 @@ Route::put('items/{item}', [ItemController::class, 'update'])->name('items.updat
 // Delete record route
 Route::delete('items/{item}', [ItemController::class, 'destroy'])->name('items.destroy');
 
-
-// CAMPAIGNS
-// List show
-Route::get('/campaigns', [CampaignController::class, 'index'])->name('campaigns.index');
-// Create record route
-Route::get('/campaigns/create', [CampaignController::class, 'create'])->name('campaigns.create');
-// Store the created record route
-Route::post('campaigns', [CampaignController::class, 'store'])->name('campaigns.store');
-// Show view route
-Route::get('campaigns/{campaign}/show', [CampaignController::class, 'show'])->name('campaigns.show');
-// Edit view route
-Route::get('campaigns/{campaign}/edit', [CampaignController::class, 'edit'])->name('campaigns.edit');
-// Update edited information route
-Route::put('campaigns/{campaign}', [CampaignController::class, 'update'])->name('campaigns.update');
-// Delete record route
-Route::delete('campaigns/{campaign}', [CampaignController::class, 'destroy'])->name('campaigns.destroy');
-
 // COMMENTS 
 Route::get('/comments/create', [CommentController::class, 'create'])->name('comments.create');
 //Store
@@ -83,14 +144,4 @@ Route::delete('comments', [CampaignController::class, 'destroy'])->name('comment
 Route::resource('users', UserController::class)->names('users');
 
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified',
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return Inertia::render('Dashboard');
-    })->name('dashboard');
 });
-
-
